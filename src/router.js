5b3183d6a8dc93ff423107b2cdd857262a900306
jgarz800 <jgarz800@stu.southtexascollege.edu>
2022-03-09 19:57:49 -0600

    Fixed uppercase J error

--- src/router.js
+++ src/router.js
@@ -4,7 +4,7 @@ import useAuth from "./composable/useAuth";
 import Index from "./pages/index.vue";
 import About from "./pages/about.vue";
 import Login from "./pages/login.vue";
-import Secret from "./pages/Secret.vue";
+import Secret from "./pages/secret.vue";
 import NotFound from "./pages/404.vue";
 
 const {isAuthenticated} = useAuth();
